6421db2fd097b999435652a4df8833b3c2297503
Xxelll <59962592+Xxelll@users.noreply.github.com>
2020-02-04 20:28:13 +0100

    Create master.c

---
+++ master.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int decisionJoueur=0;
+  
+}
+int main() {
+    printf("Pour ouvrir l'inventaire tapez : 1 \n");
+    scanf("%d",&decisionJoueur);
+    if(decisionJoueur == 1){
+			printf("\n Vous ouvrez votre inventaire\n");
+    }
+
+    return 0;
+}
