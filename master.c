5464f9087f98db23e6a2eb5764f8bd10ff8e626e
Xxelll <59962592+Xxelll@users.noreply.github.com>
2020-02-04 21:41:25 +0100

    Update master.c

--- master.c
+++ master.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
 int decisionJoueur=0;
-  
-}
+int item1 =1;
+
+struct inventaire {
+    char name[50];
+    int prix;
+
+} i;
 int main() {
+
+
     printf("Pour ouvrir l'inventaire tapez : 1 \n");
     scanf("%d",&decisionJoueur);
     if(decisionJoueur == 1){
 			printf("\n Vous ouvrez votre inventaire\n");
     }
+    printf("%s", i);
+
+    printf("Entrer informations:\n");
+    printf("Entrer nom : ");
+    fgets(i.name, sizeof(i.name), stdin);
+    printf("Entrer prix: ");
+    scanf("%d", &i.prix);
+    printf("Displaying Information:\n");
+    printf("Nom de l'item : ");
+    printf("%s", i.name);
+    printf("Prix: %d euros\n", i.prix);
 
     return 0;
 }
